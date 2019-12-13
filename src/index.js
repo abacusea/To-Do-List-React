b7f4498f479c5471fa7979dc3aec5c52af0aeed8
abacusea <55495252+abacusea@users.noreply.github.com>
2019-12-13 12:00:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
-import rootReducer from './reducers/index'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-{ /* Use compose just for enabling the redux devTools Extension */}
-
-const store = compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)(rootReducer)
-
-{ /* Provider component from react-redux library is responsive to connect 
-everything from react and redux. */}
-
-ReactDOM.render(<Provider store={store}> <App /> </Provider>,
- document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
 
